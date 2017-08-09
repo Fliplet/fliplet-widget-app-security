d787fb42bde7bb663b392e23414c76293c3f48be
Ricardo Seromenho <ricardo@seromenho.com>
2017-08-09 18:52:54 +0100

    Turn widget into a provider
    - Stores directly on app settings
    - Added new custom condition
    - Added new Data Source as a requirement
    - Possibility to add/edit multiple hooks for the app

--- js/interface.js
+++ js/interface.js
@@ -1,5 +1,5 @@
-var data = Fliplet.Widget.getData() || {};
-data.hooks = data.hooks || {};
+// Selectors
+var $accordionContainer = $('#accordion');
 
 // TODO: For now we will pick just the first hook
 var rule = {
@@ -11,108 +11,73 @@ var rule = {
   onErrorAction: null
 };
 
-// Preload data
-var hooksNames = Object.keys(data.hooks);
-if (hooksNames.length) {
-  rule = data.hooks[hooksNames[0]];
-  rule.pages = rule.pages || [];
-  $('#name').val(hooksNames[0]);
-  $('#filterType').val(rule.filterType);
-  $('#requirement').val(rule.requirement);
-  $('#errorMessage').val(rule.errorMessage);
-}
+updateSelectText($('[data-name="requirement"]'));
+updateSelectText($('[data-name="filterType"]'));
+updateSelectText($('[data-name="hookType"]'));
 
-rule.onErrorAction = rule.onErrorAction || {};
-rule.onErrorAction.action = 'screen';
-rule.onErrorAction.options = { hideAction: true };
-
-var onErrorActionProvider = Fliplet.Widget.open('com.fliplet.link', {
-  selector: '#onErrorAction',
-  data: rule.onErrorAction
-});
-
-onErrorActionProvider.then(function (result) {
-  rule.onErrorAction = result && result.data;
-  $('form').submit();
-})
-
-updateSelectText($('#requirement'));
-updateSelectText($('#filterType'));
+var i = 1; // Just a counter to have a simple unique id for each hook when rendering the template.
+var onErrorActionProviders = {};
+var hooks;
+var pages;
+var accordionCollapsed;
 
 Fliplet().then(function () {
-  Fliplet.Pages.get().then(function (pages) {
-    var contextPages = pages.map(function(page) {
-      page.selected = rule.pages.indexOf(page.id) > -1;
-      return page;
-    });
-    var pagesHtml = Fliplet.Widget.Templates['templates.pages']({pages: contextPages});
-    $('#pages').html(pagesHtml);
-    $('.selectpicker').selectpicker('render');
-  });
-
-  $('form').submit(function (event) {
-    event.preventDefault();
-
-    // Unset retreived hooks right after set them back
-    Promise.all(Object.keys(data.hooks).map(function(hook) {
-      return Fliplet.App.Hooks.unset(hook);
-    }))
-    .then(function () {
-        // Reset hooks to set them back again on the instance settings
-      data.hooks = {};
-
-      // Get selected pages
-      var pages =[];
-      $.each($(".selectpicker option:selected"), function() {
-        pages.push(Number($(this).val()));
+  Promise.all([
+    Fliplet.App.Hooks.get(),
+    Fliplet.Pages.get()
+  ])
+    .then(function(values) {
+      hooks = values[0];
+      pages = values[1];
+      Object.keys(hooks).forEach(function(hookName) {
+        var hook = hooks[hookName];
+        hook.settings = hook.settings || {};
+        hook.settings.name = hookName;
+        if (hook.settings.pages) {
+          hook.settings.pages = pages.map(function(page) {
+            page.selected = hook.settings.pages.indexOf(page.id) > -1;
+            return page;
+          });
+        } else {
+          hook.settings.pages = pages;
+        }
+        addHookItem(hook.settings);
       });
-
-      // TODO: We are setting only one for now
-      var hookName = $('#name').val() || 'My Hook ' + (new Date()).getTime().toString().substring(9);
-      $('#name').val(hookName);
-      data.hooks[hookName] = {
-        filterType: $('#filterType').val(),
-        requirement: $('#requirement').val(),
-        errorMessage: $('#errorMessage').val(),
-        onErrorAction: rule && rule.onErrorAction || null,
-        pages: pages
-      };
-
-      Fliplet.Widget.save(data).then(function () {
-        return Promise.all(Object.keys(data.hooks).map(function (name) {
-          var script = compile(data.hooks[name]);
-          console.log('saving', script)
-          return Fliplet.App.Hooks.set(name, { script: script, run: ['beforePageView'] });
-        }))
-      })
-      .then(function () {
-        Fliplet.Widget.complete();
-      })
-      .catch(function(reason) {
-        console.log(reason);
-      });
+      Fliplet.Widget.autosize();
     });
-  });
 
-  // Fired from Fliplet Studio when the external save button is clicked
-  Fliplet.Widget.onSaveRequest(function () {
-    onErrorActionProvider.forwardSaveRequest();
-  });
-
-  Fliplet.Widget.autosize();
 });
 
-function compile(hook) {
-  var comparison = hook.filterType === 'whitelist' ? '>' : '===';
-  return [
-    'if ([' + hook.pages + '].indexOf(page.id) ' + comparison + ' -1 && ',
-    '(!session || !session.server.passports.' + hook.requirement + '))',
-    '{',
-      'error = "' + hook.errorMessage + '";',
-      'action = ' + (hook.onErrorAction.action ? '"' + hook.onErrorAction.action + '"' : 'null') + ';',
-      'payload = ' + (hook.onErrorAction.page ? '{ pageId: ' + hook.onErrorAction.page + ' }' : 'null') + ';',
-    '}'
-  ].join('');
+function addHookItem(settings) {
+  settings = settings || {};
+  settings.pages = settings.pages || pages;
+  i = i + 1;
+  var extraContext = {
+    id: i,
+    beforeDataSourceQuery: settings.hookType === 'beforeDataSourceQuery',
+    beforePageView: settings.hookType === 'beforePageView',
+    whitelist: settings.filterType === 'whitelist',
+    blacklist: settings.filterType === 'blacklist',
+    saml2: settings.requirement === 'saml2',
+    dataSource: settings.requirement === 'dataSource',
+    custom: settings.requirement === 'custom'
+  };
+  var context = $.extend({}, settings, extraContext);
+  var $hook = $(Fliplet.Widget.Templates['templates.hook'](context));
+  $accordionContainer.append($hook);
+  
+  settings.onErrorAction = settings.onErrorAction || {};
+  settings.onErrorAction.action = 'screen';
+  settings.onErrorAction.options = { hideAction: true };
+  onErrorActionProviders[i] = Fliplet.Widget.open('com.fliplet.link', {
+    selector: '#accordion [data-id=' + i + '] .onErrorAction',
+    data: settings.onErrorAction
+  });
+
+  updateSelectText($hook.find('[data-name="requirement"]'));
+  updateSelectText($hook.find('[data-name="filterType"]'));
+  updateSelectText($hook.find('[data-name="hookType"]'));
+  $('.selectpicker').selectpicker('render');
 }
 
 function updateSelectText($el) {
@@ -120,6 +85,173 @@ function updateSelectText($el) {
   $el.parents('.select-proxy-display').find('.select-value-proxy').html(selectedText);
 }
 
-$('#filterType, #requirement').on('change', function () {
+$('#filterType, #requirement, #hookType').on('change', function () {
   updateSelectText($(this));
 });
+
+$('[data-name="filterType"]').on('change', function () {
+
+});
+
+$('[data-name="requirement"]').on('change', function () {
+  if ($(this).val() === 'custom') {
+    $('[data-name="customCondition"]').show();
+  }
+});
+
+// Listeners
+$accordionContainer
+  .on('click', '.icon-delete', function() {
+    var $item = $(this).closest("[data-id], .panel");
+    $item.remove();
+  })
+  .on('keyup change paste', '[data-name="hookName"]', function() {
+    $(this).parents('.panel').find('.panel-title-text').html(this.value);
+  })
+  .on('show.bs.collapse', '.panel-collapse', function() {
+    $(this).siblings('.panel-heading').find('.fa-chevron-right').removeClass('fa-chevron-right').addClass('fa-chevron-down');
+    Fliplet.Widget.autosize
+  })
+  .on('hide.bs.collapse', '.panel-collapse', function() {
+    $(this).siblings('.panel-heading').find('.fa-chevron-down').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+    Fliplet.Widget.autosize
+  })
+  .on('shown.bs.collapse hidden.bs.collapse', '.panel-collapse', function() {
+    $('.tab-content').trigger('scroll');
+    Fliplet.Widget.autosize
+  })
+  .on('change', '[data-name="hookType"]', function () {
+    if ($(this).val() === 'beforePageView') {
+      $(this).closest('.panel').find('.filter-type').show();
+      $(this).closest('.panel').find('.pages').show();
+    }
+    if ($(this).val() === 'beforeDataSourceQuery') {
+      $(this).closest('.panel').find('.filter-type').hide();
+      $(this).closest('.panel').find('.pages').hide();
+    }
+  })
+  .on('change', '[data-name="requirement"]', function () {
+    if ($(this).val() === 'custom') {
+      $(this).closest('.panel').find('.custom-condition').show();
+    } else {
+      $(this).closest('.panel').find('.custom-condition').hide();
+    }
+  });
+  
+$('.expand-items').on('click', function() {
+  var $panelCollapse = $('.panel-collapse.in');
+  // Update accordionCollapsed if all panels are collapsed/expanded
+  if (!$panelCollapse.length) {
+    accordionCollapsed = true;
+  } else if ($panelCollapse.length == $('.panel-collapse').length) {
+    accordionCollapsed = false;
+  }
+
+  if (accordionCollapsed) {
+    expandAccordion();
+  } else {
+    collapseAccordion();
+  }
+});
+
+function expandAccordion() {
+  accordionCollapsed = false;
+  $('.panel-collapse').collapse('show');
+}
+
+function collapseAccordion() {
+  accordionCollapsed = true;
+  $('.panel-collapse').collapse('hide');
+}
+
+  $('.new-hook').on('click', function() {
+    addHookItem();
+  });
+
+Fliplet.Widget.onSaveRequest(function() {
+  var newHooks = {};
+  
+  // Create a hook for each panel
+  $('[data-id]').each(function() {
+    var id = $(this).data('id');
+
+    // Get selected pages
+    var pages =[];
+    $.each($(this).find(".selectpicker option:selected"), function() {
+      pages.push(Number($(this).val()));
+    });
+
+    var hookName = $(this).find('[data-name=hookName]').val() || 'My Hook ' + (new Date()).getTime().toString().substring(9);
+    $(this).find('[data-name=hookName]').val(hookName);
+    newHooks[hookName] = {};
+    newHooks[hookName].settings = {
+      name: hookName,
+      hookType: $(this).find('[data-name=hookType]').val(),
+      filterType: $(this).find('[data-name=filterType]').val(),
+      requirement: $(this).find('[data-name=requirement]').val(),
+      errorMessage: $(this).find('[data-name=errorMessage]').val(),
+      customCondition: $(this).find('[data-name=customCondition]').val(),
+      pages: pages
+    };
+    newHooks[hookName].run = [$(this).find('#filterType').val()];
+    
+    onErrorActionProviders[id].then(function (result) {
+      newHooks[hookName].settings.onErrorAction = result && result.data;
+      newHooks[hookName].script = compile(newHooks[hookName].settings);
+    });
+  });
+
+  Promise.all(Object.keys(onErrorActionProviders).map(function(id) {
+    return onErrorActionProviders[id].forwardSaveRequest();
+  }))
+  .then(function () {
+    return Fliplet.API.request({
+      url: 'v1/apps/' + Fliplet.Env.get('appId'),
+      method: 'PUT',
+      data: {
+        hooks: newHooks
+      }
+    });
+  });
+});
+
+/**
+ * Given settings this creates a script that will run on the set hook.
+ * @param {Object} hook 
+ */
+function compile(hook) {
+  if (hook.hookType === 'beforePageView') {
+    var comparison = hook.filterType === 'whitelist' ? '>' : '===';
+    return [
+      'if ([' + hook.pages + '].indexOf(page.id) ' + comparison + ' -1 && ',
+      '(!session || !session.server.passports.' + hook.requirement + ')',
+      hook.customCondition ? ' && ' + hook.customCondition : '',
+      ')',
+      '{',
+        'error = "' + hook.errorMessage + '";',
+        'action = ' + (hook.onErrorAction.action ? '"' + hook.onErrorAction.action + '"' : 'null') + ';',
+        'payload = ' + (hook.onErrorAction.page ? '{ pageId: ' + hook.onErrorAction.page + ' }' : 'null') + ';',
+      '}'
+    ].join('');
+  }
+
+  if (hook.hookType === 'beforeDataSourceQuery') {
+    var condition;
+    if (hook.requirement === 'custom') {
+      condition = hook.customCondition;
+    } else {
+      condition = '!session || !session.server.passports.' + hook.requirement;
+      // Also add custom condition if set even with a passport requirement
+      if (hook.customCondition) {
+        condition = condition + ' && ' + hook.customCondition
+      } 
+    }
+    
+    return [
+      'if (' + condition + ')',
+      '{',
+        'error = "' + hook.errorMessage || 'Secured query' + '";',
+      '}'
+    ].join('');
+  } 
+}
--- js/interface.templates.js
+++ js/interface.templates.js
@@ -2,22 +2,52 @@ this["Fliplet"] = this["Fliplet"] || {};
 this["Fliplet"]["Widget"] = this["Fliplet"]["Widget"] || {};
 this["Fliplet"]["Widget"]["Templates"] = this["Fliplet"]["Widget"]["Templates"] || {};
 
-this["Fliplet"]["Widget"]["Templates"]["templates.pages"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
+this["Fliplet"]["Widget"]["Templates"]["templates.hook"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
+    return "selected ";
+},"3":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "    <option "
-    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.selected : depth0),{"name":"if","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+  return "              <option "
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.selected : depth0),{"name":"if","hash":{},"fn":container.program(4, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + " value=\""
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
     + "\">"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
     + "</option>\n";
-},"2":function(container,depth0,helpers,partials,data) {
+},"4":function(container,depth0,helpers,partials,data) {
     return " selected ";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1;
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<select class=\"selectpicker\" data-width=\"auto\" multiple>\n"
-    + ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.pages : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "</select>";
+  return "<div class=\"panel panel-default\" data-id=\""
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "\">\n  <div class=\"panel-heading ui-sortable-handle\">\n    <h4 class=\"panel-title\" data-toggle=\"collapse\" data-parent=\"#accordion\" data-target=\"#collapse-"
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "\">\n      <div class=\"screen-reorder-handle\">\n        <i class=\"fa fa-ellipsis-v\"></i><i class=\"fa fa-ellipsis-v\"></i>\n      </div>\n      <span class=\"fa fa-chevron-right\"></span>\n      <span class=\"panel-title-text\">"
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</span>\n    </h4>\n    <a href=\"#\"><span class=\"icon-delete fa fa-trash\"></span></a>\n  </div>\n\n  <div id=\"collapse-"
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "\" class=\"panel-collapse collapse\">\n    <div class=\"panel-body\">\n\n      <div class=\"form-group\">\n        <div class=\"col-sm-4 control-label\">\n          <label for=\"sso_logout_url\">Rule name</label>\n        </div>\n        <div class=\"col-sm-8\">\n          <input data-name=\"hookName\" type=\"text\" class=\"form-control\" placeholder=\"My hook name\" value=\""
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "\" required/>\n          <h6>A unique identifier name for your rule .</h6>\n        </div>\n      </div>\n\n      <div class=\"form-group clearfix\">\n        <div class=\"col-sm-4 control-label\">\n          <label for=\"hookType\">Apply hook on:</label>\n        </div>\n        <div class=\"col-sm-8\">\n          <label for=\"hookType\" class=\"select-proxy-display\">\n            <span class=\"icon fa fa-chevron-down\"></span>\n            <span class=\"select-value-proxy\">Select a type</span>\n            <select data-name=\"hookType\" data-label=\"select\" class=\"hidden-select form-control\" required>\n              <option value=\"beforePageView\" "
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.beforePageView : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ">&mdash; Before page view &mdash;</option>\n              <option value=\"beforeDataSourceQuery\" "
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.beforeDataSourceQuery : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ">&mdash; Before data source query &mdash;</option>\n            </select>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"form-group clearfix filter-type\">\n        <div class=\"col-sm-4 control-label\">\n          <label for=\"filterType\">Filter type</label>\n        </div>\n        <div class=\"col-sm-8\">\n          <label for=\"filterType\" class=\"select-proxy-display\">\n            <span class=\"icon fa fa-chevron-down\"></span>\n            <span class=\"select-value-proxy\">Select a type</span>\n            <select data-name=\"filterType\" data-label=\"select\" class=\"hidden-select form-control\" required>\n              <option value=\"blacklist\" "
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.blacklist : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ">&mdash; Blacklist &mdash;</option>\n              <option value=\"whitelist\" "
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.whitelist : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ">&mdash; Whitelist &mdash;</option>\n            </select>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"form-group clearfix pages\">\n        <div class=\"col-sm-4 control-label\">\n          <label for=\"pages\">Pages</label>\n        </div>\n        <div class=\"col-sm-8\">\n          <select class=\"selectpicker\" data-width=\"auto\" multiple>\n"
+    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.pages : depth0),{"name":"each","hash":{},"fn":container.program(3, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "          </select>\n        </div>\n      </div>\n\n      <div class=\"form-group clearfix requirement\">\n        <div class=\"col-sm-4 control-label\">\n          <label for=\"requirement\">Requirement</label>\n        </div>\n        <div class=\"col-sm-8\">\n          <label for=\"requirement\" class=\"select-proxy-display\">\n            <span class=\"icon fa fa-chevron-down\"></span>\n            <span class=\"select-value-proxy\">Select a type</span>\n            <select data-name=\"requirement\" data-label=\"select\" class=\"hidden-select form-control\" required>\n              <option value=\"saml2\" "
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.saml2 : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ">&mdash; SAML2 &mdash;</option>\n              <option value=\"dataSource\" "
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.dataSource : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ">&mdash; Data source &mdash;</option>\n              <option value=\"custom\" "
+    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.custom : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + ">&mdash; Custom condition &mdash;</option>\n            </select>\n          </label>\n        </div>\n      </div>\n\n      <div class=\"form-group clearfix\">\n        <div class=\"col-sm-4 control-label\">\n          <label for=\"sso_logout_url\">Error message</label>\n        </div>\n        <div class=\"col-sm-8\">\n          <input type=\"text\" class=\"form-control\" data-name=\"errorMessage\" value=\""
+    + alias4(((helper = (helper = helpers.errorMessage || (depth0 != null ? depth0.errorMessage : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"errorMessage","hash":{},"data":data}) : helper)))
+    + "\" required/>\n          <h6>An error message to present to the user when the hook is rejecting.</h6>\n        </div>\n      </div>\n\n      <div class=\"form-group clearfix custom-condition\">\n        <div class=\"col-sm-4 control-label\">\n          <label for=\"sso_logout_url\">Custom condition</label>\n        </div>\n        <div class=\"col-sm-8\">\n          <input type=\"text\" class=\"form-control\" data-name=\"customCondition\" value=\""
+    + alias4(((helper = (helper = helpers.customCondition || (depth0 != null ? depth0.customCondition : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"customCondition","hash":{},"data":data}) : helper)))
+    + "\" required/>\n          <h6>A custom condition set by you that will trigger the error.</h6>\n        </div>\n      </div>\n\n      <div class=\"form-group clearfix\">\n        <div class=\"col-sm-12\">\n          <h3>Action on error</h4>\n          <div class=\"onErrorAction\"></div>\n        </div>\n      </div>\n    </div>\n  </div>\n</div>";
 },"useData":true});
